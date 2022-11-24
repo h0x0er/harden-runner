e8ddafd03f57b2a8e7541fb8d659727a9d7a63da
h0x0er <jatink843@protonmail.com>
2022-11-24 20:50:33 +0530

    changed monitor url

--- src/setup.ts
+++ src/setup.ts
@@ -86,7 +86,7 @@ import {
       _http.requestOptions = { socketTimeout: 3 * 1000 };
       try {
         await _http.get(
-          `${api_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}/monitor`
+          `${api_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}/monitorr`
         );
       } catch (e) {
         console.log(`error in connecting to ${api_url}: ${e}`);
