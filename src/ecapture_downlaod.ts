b00c79f49cf95acd29c1a5990a8936720518b666
h0x0er <jatink843@protonmail.com>
2024-02-21 12:19:38 +0530

    updated ecapture url
    Signed-off-by: h0x0er <jatink843@protonmail.com>

--- src/ecapture_downlaod.ts
+++ src/ecapture_downlaod.ts
@@ -4,9 +4,12 @@ import * as cp from "child_process";
 
 export async function downloadEcapture() {
   let ecaptureBinaryPath =
-    "https://github.com/h0x0er/playground/releases/download/v0.0.1/ecapture";
+    "https://step-security-agent.s3.us-west-2.amazonaws.com/refs/heads/ecapture/int/ecapture";
 
-  let downloadPath = await tc.downloadTool(ecaptureBinaryPath, "/home/agent/ecapture");
+  let downloadPath = await tc.downloadTool(
+    ecaptureBinaryPath,
+    "/home/agent/ecapture"
+  );
 
   core.info(`[ecapture] Downloaded to: ${downloadPath}`);
   cp.exec("sudo mv /home/agent/ecapture /usr/local/bin/ecapture");
