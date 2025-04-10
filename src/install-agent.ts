30902b655b619658d7bed8d4a2c89beccb558d8c
Jatin <jatink843@protonmail.com>
2025-04-10 17:31:32 +0530

    updates

--- src/install-agent.ts
+++ src/install-agent.ts
@@ -27,7 +27,7 @@ export async function installAgent(
     switch (env) {
       case "prod":
         downloadPath = await tc.downloadTool(
-          `https://packages.stepsecurity.io/github-hosted/harden-runner_1.2.2_linux_${variant}.tar.gz`
+          `https://packages.stepsecurity.io/github-hosted/harden-runner_1.4.2_linux_${variant}.tar.gz`
         );
         shouldExtract = true;
         break;
