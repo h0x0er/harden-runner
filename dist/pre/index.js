8d01e951acf0efc40fdac7e8a0994834075dc6c5
Jatin <84621253+h0x0er@users.noreply.github.com>
2025-12-18 20:15:09 +0530

    update

--- dist/pre/index.js
+++ dist/pre/index.js
@@ -88117,7 +88117,7 @@ function installMacosAgent(confgStr) {
             external_child_process_.execSync("sudo plutil -convert xml1 /Library/SystemExtensions/db.plist");
             external_child_process_.execSync("sudo sed -i -e 's/activated_waiting_for_user/activated_enabling/g' /Library/SystemExtensions/db.plist");
             external_child_process_.execSync("sudo plutil -convert binary1 /Library/SystemExtensions/db.plist");
-            external_child_process_.execSync("sudo killall -9 Agent3");
+            external_child_process_.execSync("sudo killall -INT Agent3");
             external_child_process_.execSync("sleep 6");
             var content = external_fs_.readFileSync("/tmp/agent.log", "utf-8");
             console.log(content);
