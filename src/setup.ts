e2f5a28be170eb4b96d6c16e8125f34974db9638
Jatin <84621253+h0x0er@users.noreply.github.com>
2026-01-15 14:06:35 +0530

    refactor: adjusted delay

--- src/setup.ts
+++ src/setup.ts
@@ -391,7 +391,7 @@ interface MonitorResponse {
         }
         console.log("waiting for 5 seconds");
         cp.execSync(
-          "curl --retry 4 --retry-delay 1 --max-time 1 https://int1.stepsecurity.io || true"
+          "curl --retry 1 --retry-delay 1 --max-time 2 https://int1.stepsecurity.io || true"
         );
     }
   } catch (error) {
